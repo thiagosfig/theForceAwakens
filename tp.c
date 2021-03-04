7cff3b26896b9c274c55804da727640e182c833e
thiagosfig <thiagosfig@gmail.com>
2021-03-04 20:52:42 -0300

    add exemplo combinacao

--- tp.c
+++ tp.c
@@ -2,6 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 
+int counter;
+
+void combinationUtil(int arr[], int data[], int start, int end, int index, int r)
+{
+    int i, j;
+
+    if (index == r)
+    {
+        for (j = 0; j < r; j++)
+            printf("%d ", data[j]);
+        counter++;
+        printf("\n");
+        return;
+    }
+
+    for (i = start; i <= end && end - i + 1 >= r - index; i++)
+    {
+        data[index] = arr[i];
+        combinationUtil(arr, data, i + 1, end, index + 1, r);
+    }
+}
+
+void printCombination(int arr[], int n, int r)
+{
+    int data[r];
+
+    combinationUtil(arr, data, 0, n - 1, 0, r);
+}
+
 void dynamicProg(int n, int k, int *a)
 {
 
@@ -123,9 +152,17 @@ void readEntry(int type)
 int main(int argc, char **argv)
 {
 
-    int type = readType(argv);
+    //int type = readType(argv);
 
-    readEntry(type);
+    //readEntry(type);
+
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int r = 3;
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printCombination(arr, n, r);
+
+    printf("%d", counter);
 
     return 0;
 }
