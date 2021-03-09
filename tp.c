56d244aed4ae5eb4c6953174e519d9da8fae02de
thiagosfig <thiagosfig@gmail.com>
2021-03-09 10:10:48 -0300

    Adicionado comentários - Forca Bruta OKAY

--- tp.c
+++ tp.c
@@ -4,7 +4,7 @@
 
 int minMax;
 
-void combinationUtil(int *path, int arr[], int data[], int start, int end, int index, int r, int n)
+void combinationUtil(int *path, int comb[], int data[], int start, int end, int index, int k, int n)
 {
     int pathSize = n;
     int i, j;
@@ -13,9 +13,9 @@ void combinationUtil(int *path, int arr[], int data[], int start, int end, int i
     int custoViagem = 0;
     int maxLocal = 0;
 
-    if (index == r)
+    if (index == k)
     {
-        for (j = 0; j < r; j++)
+        for (j = 0; j < k; j++)
         {
             posDestino = data[j];
             // printf("\nSaindo de %d para %d -> ", posAtual, posDestino);
@@ -45,19 +45,20 @@ void combinationUtil(int *path, int arr[], int data[], int start, int end, int i
         return;
     }
 
-    for (i = start; i <= end && end - i + 1 >= r - index; i++)
+    for (i = start; i <= end && end - i + 1 >= k - index; i++)
     {
-        data[index] = arr[i];
-        combinationUtil(path, arr, data, i + 1, end, index + 1, r, n);
+        data[index] = comb[i];
+        combinationUtil(path, comb, data, i + 1, end, index + 1, k, n);
     }
 }
 
-void Combination(int *path, int arr[], int n, int r)
+void Combination(int *path, int comb[], int n, int k)
 {
-    int data[r];
+    //data eh um arranjo com k elementos
+    int data[k];
     minMax = 9999;
 
-    combinationUtil(path, arr, data, 0, n - 1, 0, r, n);
+    combinationUtil(path, comb, data, 0, n - 1, 0, k, n);
 
     printf("\n%d", minMax);
 
@@ -92,9 +93,11 @@ void greedyAlg(int n, int k, int *a)
     return;
 }
 
-void bruteForce(int n, int k, int *a)
+// Estrategia de foca bruta
+void bruteForce(int n, int k, int *rota)
 {
 
+    //comb eh um vetor de 1 a n planetas
     int *comb = (int *)calloc(n, sizeof(int));
 
     for (int i = 1; i < n + 1; i++)
@@ -102,45 +105,51 @@ void bruteForce(int n, int k, int *a)
         comb[i - 1] = i;
     }
 
-    Combination(a, comb, n, k);
+    Combination(rota, comb, n, k);
 
     free(comb);
 
     return;
 }
 
+//Funcao para ler o tipo de estrategia
 int readType(char **argv)
 {
 
-    char x[3];
+    char type[3];
     char PD[] = "PD";
     char AG[] = "AG";
     char FB[] = "FB";
 
-    sscanf(argv[1], "%s", x);
+    sscanf(argv[1], "%s", type);
 
-    if (strcmp(x, PD) == 0)
+    if (strcmp(type, PD) == 0)
     {
         printf("Você optou por PROGRAMAÇÃO DINÂMICA\n");
         return 1;
     }
-    if (strcmp(x, AG) == 0)
+    if (strcmp(type, AG) == 0)
     {
         printf("Você optou por ALGORITMO GULOSO\n");
         return 2;
     }
-    if (strcmp(x, FB) == 0)
+    if (strcmp(type, FB) == 0)
     {
         printf("Você optou por FORÇA BRUTA\n");
         return 3;
     }
 }
 
+//Ler entrada do usuario (t n k a1 a2 ... an)
+//t - quantiade de instancias do problema
+//n - quantidade de planetas de uma instancia
+//k - quatidade de planetas a ser conquistado
+//a1, a2, ..., an - custo da viagem entre planetas -> I p/ P1 = a1 .... an p/ F = an
 void readEntry(int type)
 {
 
     int t, n, k, a;
-    int *ptr;
+    int *rota;
     printf("Digite sua entrada: ");
     scanf("%d", &t);
 
@@ -148,9 +157,9 @@ void readEntry(int type)
     {
         scanf("%d %d", &n, &k);
 
-        ptr = (int *)calloc(n + 1, sizeof(int));
+        rota = (int *)calloc(n + 1, sizeof(int));
 
-        if (ptr == NULL)
+        if (rota == NULL)
         {
             printf("Memory not allocated.\n");
             exit(0);
@@ -161,35 +170,37 @@ void readEntry(int type)
             for (int i = 0; i < n + 1; ++i)
             {
                 scanf("%d", &a);
-                ptr[i] = a;
+                rota[i] = a;
             }
         }
 
         switch (type)
         {
         case 1:
-            dynamicProg(n, k, ptr);
+            dynamicProg(n, k, rota);
             break;
         case 2:
-            greedyAlg(n, k, ptr);
+            greedyAlg(n, k, rota);
             break;
         case 3:
-            bruteForce(n, k, ptr);
+            bruteForce(n, k, rota);
             break;
         default:
             printf("\nDEU BIZIU!!!\n");
             break;
         }
 
-        free(ptr);
+        free(rota);
     }
 }
 
 int main(int argc, char **argv)
 {
 
+    //Ler o tipo de estrategia a ser adotada
     int type = readType(argv);
 
+    //Ler entradas do usuario
     readEntry(type);
 
     return 0;
